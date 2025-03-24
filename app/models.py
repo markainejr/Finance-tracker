29476fe77bff02cffb2caabc419b74a7cf06b863
Peter Kure <84036156+peterkure3@users.noreply.github.com>
2025-03-24 04:55:17 -0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -9,3 +9,14 @@ class Transaction(db.Model):
 
     def __repr__(self):
         return f'<Transaction {self.description}>'
+
+class User(db.Model):
+    __ tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    created_at = db.Column(db.TIMESTAMP(timezone=True), server_default=text('now()'))
+    transactions = db.relationship('Transaction', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f'<User {self.username}>'
