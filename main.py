e51f8036a5cb4a54fa141f5676a727fe80a290eb
Peter Kure <84036156+peterkure3@users.noreply.github.com>
2025-03-24 04:49:36 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,15 @@
 from flask import Flask
-
+from config import Config
+from database import db, init_db
+from models import User, Transaction
 
 app = Flask(__name__)
+app.config.from_object(Config)
 
-
-
-@app.route('/')
-def check():
-  return 'Hello world'
-
-
-
-
-
-
+init_db(app)
 
 with app.app_context():
+  db.create_all()
 
- if __name__ == '__main__':
+if __name__ == '__main__':
     app.run(debug=True)
