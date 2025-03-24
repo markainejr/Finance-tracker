2afd8742aafc41e15e293c33278f019bdf3bc731
Peter Kure <84036156+peterkure3@users.noreply.github.com>
2025-03-24 05:13:46 -0700

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,11 +1,13 @@
 from database import db
-
+from sqlachemy import text
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     description = db.Column(db.String(100), nullable=False)
     amount = db.Column(db.Float, nullable=False)
     date = db.Column(db.DateTime, nullable=False)
     category = db.Column(db.String(50))  # New field
+    created_at = db.Column(db.TIMESTAMP(timezone=True), server_default=text('now()'))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
     def __repr__(self):
         return f'<Transaction {self.description}>'
